99a7880be0eeb94f77f1aa94380e890e89b9da52
Josh Riches <joshriches@gmail.com>
2018-06-05 15:46:09 -0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const Senators = require('./data/senators');
+const Hapi = require('hapi');
+// const { get } = require('hapi');
+// const { router } = require('hapi-router');
+
+'use strict';
+
+const server = Hapi.server ({
+    port: 3000,
+    host: 'localhost'
+});
+
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+        return 'Hello World';
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/all',
+    handler: (request, h) => {
+        return Senators;
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/indies',
+    handler: (request, h) => {
+        return Senators.filter(senator => senator.party === 'Independent')
+    }
+})
+
+
+
+const init = async () => {
+    await server.start();
+    console.log(`Server Running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
